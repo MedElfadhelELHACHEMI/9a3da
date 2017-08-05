5d610d66806b79461659d82a5b853a3cb5f79c8f
Mohamed el fadhel el hachemi <mohamedelfadhel.elhachemi@esprit.tn>
2017-08-05 23:32:31 +0100

    redo

--- client/app/components/nightoutphotos/nightoutphotos.controller.js
+++ client/app/components/nightoutphotos/nightoutphotos.controller.js
@@ -1,6 +1,16 @@
 class NightoutphotosController {
   constructor() {
     this.name = 'nightoutphotos';
+    this.selectedPhoto='';
+    this.modal=false;
+  }
+  $onInit(){
+    console.log(this.photos)
+  }
+  dick($index){
+    console.log(this.photos[$index].id)
+    this.selectedPhoto=this.photos[$index];
+    this.modal=true
   }
 }
 
